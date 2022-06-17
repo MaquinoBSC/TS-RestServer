d08f87aa3a191b83fe58cc98fb087219e560ec42
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-16 21:42:30 -0500

    Se crea el servidor con express y TS

--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-export const nombre= 'Maquino en TS';
-console.log(nombre);
- 
+import dotenv from 'dotenv';
+import Server from './models/server';
+
+// Configurar dotenv
+dotenv.config();
+
+// Crear y levantar servidor
+const server= new Server();
+server.listen();
