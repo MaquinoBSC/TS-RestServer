9528fd9821607a1385bcb203d7ae477e1a994219
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-16 21:55:37 -0500

    Se compila app para ejemplificar el uso de nodemon dis/app.js y tsc --watch

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.nombre = void 0;
-exports.nombre = 'Maquino en TS';
-console.log(exports.nombre);
+const dotenv_1 = __importDefault(require("dotenv"));
+const server_1 = __importDefault(require("./models/server"));
+// Configurar dotenv
+dotenv_1.default.config();
+// Crear y levantar servidor
+const server = new server_1.default();
+server.listen();
 //# sourceMappingURL=app.js.map
